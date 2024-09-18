9823d70d243d00244cc8beeac5dd51ad09940311
Gabriella Muniz <gabriellamunizzz08@gmail.com>
2024-09-18 11:26:57 -0300

    Atualizações de projetos

--- src/components/ProjetosInicial.vue
+++ src/components/ProjetosInicial.vue
@@ -13,10 +13,11 @@
     </p>
 
     <ul
-      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px] sm:gap-[30px] justify-items-center mt-[50px] sm:mt-[100px]"
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-[20px] sm:gap-[30px] justify-items-center mt-[50px] sm:mt-[100px]"
     >
-      <!-- Projeto 1 -->
-      <li
+
+    <!-- Projeto 1 -->
+    <li
         class="bg-[#2c323b] flex flex-col justify-between items-center w-full max-w-[300px] sm:max-w-[350px] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105"
       >
         <div class="h-[150px] sm:h-[200px] overflow-hidden">
@@ -26,25 +27,23 @@
           </video>
         </div>
         <div class="p-4 sm:p-6 flex flex-col gap-[15px] sm:gap-[20px] text-center flex-grow">
-          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5]">Catpédia</h3>
+          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5] font-font">
+            Reading Diary
+          </h3>
           <p
-            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7] text-justify font-font2"
+            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7]  font-font2"
           >
-            Catpédia é um aplicativo web simples criado para exibir informações sobre raças de gatos
-            diferentes. As informações são obtidas de uma API pública de gatos
-            (The Cat API). O objetivo é consumir essa API e exibir os dados em uma página
-            da web de forma clara e organizada, incluindo gráficos de análise.
+          Um espaço digital onde você pode registrar todos os livros que já leu, compartilhar suas opiniões e planejar suas próximas leituras. Com o Diário de Leitura, você pode definir metas de leitura, acompanhar seu progresso e criar uma lista de desejos literária!
           </p>
         </div>
         <a
           class="bg-[#00adb5] text-[#F4F6F9] py-2 sm:py-3 px-4 sm:px-6 text-xs sm:text-sm uppercase tracking-wider font-bold rounded-md shadow-lg transition-transform duration-300 hover:translate-y-[-5px] mb-4 sm:mb-6 font-font"
-          href="https://gabriella-muniz.github.io/catpedia/"
+          href="https://gabriella-muniz.github.io/reading-diary/"
           target="_blank"
         >
           Acesse Aqui
         </a>
       </li>
-
       <!-- Projeto 2 -->
       <li
         class="bg-[#2c323b] flex flex-col justify-between items-center w-full max-w-[300px] sm:max-w-[350px] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105"
@@ -56,21 +55,19 @@
           </video>
         </div>
         <div class="p-4 sm:p-6 flex flex-col gap-[15px] sm:gap-[20px] text-center flex-grow">
-          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5]">
-            Projeto Calculadora
-          </h3>
+          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5] font-font">Catpédia</h3>
           <p
-            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7] text-justify font-font2"
+            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7]  font-font2"
           >
-            Criação de uma calculadora simples utilizando HTML, CSS e JavaScript, e também utilizei
-            a biblioteca Bootstrap. Esta calculadora possui uma interface amigável e responsiva para
-            realizar cálculos básicos. Os números e operadores são apresentados em botões Bootstrap,
-            e o resultado é exibido em um campo de entrada de texto.
+            Catpédia é um aplicativo web simples criado para exibir informações sobre raças de gatos
+            diferentes. As informações são obtidas de uma API pública de gatos
+            (The Cat API). O objetivo é consumir essa API e exibir os dados em uma página
+            da web de forma clara e organizada, incluindo gráficos de análise.
           </p>
         </div>
         <a
           class="bg-[#00adb5] text-[#F4F6F9] py-2 sm:py-3 px-4 sm:px-6 text-xs sm:text-sm uppercase tracking-wider font-bold rounded-md shadow-lg transition-transform duration-300 hover:translate-y-[-5px] mb-4 sm:mb-6 font-font"
-          href="https://gabriella-muniz.github.io/calculadora/"
+          href="https://gabriella-muniz.github.io/catpedia/"
           target="_blank"
         >
           Acesse Aqui
@@ -88,15 +85,45 @@
           </video>
         </div>
         <div class="p-4 sm:p-6 flex flex-col gap-[15px] sm:gap-[20px] text-center flex-grow">
-          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5]">
+          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5] font-font">
+            Projeto Calculadora
+          </h3>
+          <p
+            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7] font-font2"
+          >
+            Criação de uma calculadora simples utilizando HTML, CSS e JavaScript, e também utilizei
+            a biblioteca Bootstrap. Esta calculadora possui uma interface amigável e responsiva para
+            realizar cálculos básicos. 
+          </p>
+        </div>
+        <a
+          class="bg-[#00adb5] text-[#F4F6F9] py-2 sm:py-3 px-4 sm:px-6 text-xs sm:text-sm uppercase tracking-wider font-bold rounded-md shadow-lg transition-transform duration-300 hover:translate-y-[-5px] mb-4 sm:mb-6 font-font"
+          href="https://gabriella-muniz.github.io/calculadora/"
+          target="_blank"
+        >
+          Acesse Aqui
+        </a>
+      </li>
+
+      <!-- Projeto 4 -->
+      <li
+        class="bg-[#2c323b] flex flex-col justify-between items-center w-full max-w-[300px] sm:max-w-[350px] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105"
+      >
+        <div class="h-[150px] sm:h-[200px] overflow-hidden">
+          <video autoplay loop muted playsinline class="video-background">
+            <source :src="videoSrc" type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+        </div>
+        <div class="p-4 sm:p-6 flex flex-col gap-[15px] sm:gap-[20px] text-center flex-grow">
+          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5] font-font">
             Catálogo de Filmes
           </h3>
           <p
-            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7] text-justify font-font2"
+            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7]  font-font2"
           >
             Esse projeto consiste em um catálogo de filmes, desenvolvido usando tecnologias web
-            modernas, como HTML, CSS e JavaScript. A plataforma foi criada para aprimoramento das
-            minhas habilidades com tais tecnologias. Outros conceitos envolvidos neste projeto
+            modernas, como HTML, CSS e JavaScript. neste projeto
             foram: a) Tags semânticas HTML; b) CSS Grid; c) CSS Flexbox; d) Media Queries para
             garantir responsividade.
           </p>
@@ -109,18 +136,78 @@
           Acesse Aqui
         </a>
       </li>
+
+       <!-- Projeto 5 -->
+       <li
+        class="bg-[#2c323b] flex flex-col justify-between items-center w-full max-w-[300px] sm:max-w-[350px] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105"
+      >
+        <div class="h-[150px] sm:h-[200px] overflow-hidden">
+          <video autoplay loop muted playsinline class="video-background">
+            <source :src="videoSrc" type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+        </div>
+        <div class="p-4 sm:p-6 flex flex-col gap-[15px] sm:gap-[20px] text-center flex-grow">
+          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5] font-font">
+            Encontre o número (Jogo)
+          </h3>
+          <p
+            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7]  font-font2"
+          >
+          Nesse projeto o usuário tenta adivinhar um número entre 1 e 20. O jogo dá dicas se o número é maior ou menor do que o número correto e mostra o total de tentativas até o acerto. O jogo foi criado com HTML, CSS e Javascript.
+          </p>
+        </div>
+        <a
+          class="bg-[#00adb5] text-[#F4F6F9] py-2 sm:py-3 px-4 sm:px-6 text-xs sm:text-sm uppercase tracking-wider font-bold rounded-md shadow-lg transition-transform duration-300 hover:translate-y-[-5px] mb-4 sm:mb-6 font-font"
+          href="https://gabriella-muniz.github.io/jogo-encontre-o-numero/"
+          target="_blank"
+        >
+          Acesse Aqui
+        </a>
+      </li>
+
+       <!-- Projeto 6 -->
+       <li
+        class="bg-[#2c323b] flex flex-col justify-between items-center w-full max-w-[300px] sm:max-w-[350px] rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105"
+      >
+        <div class="h-[150px] sm:h-[200px] overflow-hidden">
+          <video autoplay loop muted playsinline class="video-background">
+            <source :src="videoSrc" type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+        </div>
+        <div class="p-4 sm:p-6 flex flex-col gap-[15px] sm:gap-[20px] text-center flex-grow">
+          <h3 class="font-bold text-[1.5rem] sm:text-[1.8rem] text-[#00adb5] font-font">
+            Meu Primeiro Site
+          </h3>
+          <p
+            class="text-[1rem] sm:text-[1.2rem] text-[#F4F6F9] leading-[1.7]  font-font2"
+          >
+          Esse projeto foi um desafio proposto pelo Professor Guanabara através do curso HTML 5 e CSS3 do Curso em vídeo.
+          </p>
+        </div>
+        <a
+          class="bg-[#00adb5] text-[#F4F6F9] py-2 sm:py-3 px-4 sm:px-6 text-xs sm:text-sm uppercase tracking-wider font-bold rounded-md shadow-lg transition-transform duration-300 hover:translate-y-[-5px] mb-4 sm:mb-6 font-font"
+          href="https://gabriella-muniz.github.io/desafio1-html-css/"
+          target="_blank"
+        >
+          Acesse Aqui
+        </a>
+      </li>
+
+      
     </ul>
   </section>
 </template>
 
 <script>
-import videoSrc from '../assets/projetos.mp4' // Caminho correto para o vídeo
+import videoSrc from '../assets/projetos.mp4';
 
 export default {
   name: 'ProjetosInicial',
   data() {
     return {
-      videoSrc // Referência ao vídeo importado
+      videoSrc 
     }
   }
 }
@@ -128,8 +215,8 @@ export default {
 
 <style scoped>
 .video-background {
-  width: 500px; /* Defina a largura desejada */
-  height: 190px; /* Defina a altura desejada */
-  object-fit: cover; /* Ajusta o vídeo para cobrir a área sem distorção */
+  width: 500px; 
+  height: 190px; 
+  object-fit: cover; 
 }
 </style>
